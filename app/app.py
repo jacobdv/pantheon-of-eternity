cc0d844b5c33bc692f8e702ed628e377b43838a0
Jacob de Vries <mrjacobdevries@gmail.com>
2021-06-05 13:56:43 -0700

    Adding files.

--- app/app.py
+++ app/app.py
@@ -2,8 +2,14 @@
 from flask import Flask, render_template, redirect, url_for, jsonify, request
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+import pymongo
 import tensorflow as tf
 import datetime as dt
+
+# Additional tools for API routes.
+import json
+from bson import json_util
+from bson.json_util import dumps
 
 # Pulling current date.
 date = dt.date.today()
@@ -19,33 +25,25 @@ CORS(app)
 app.config['DEBUG'] = True
 app.config['MONGO_URI'] = 'mongodb+srv://readonly:2kOAcow4qM6E4ANE@cluster0.6oig2.mongodb.net/test?authSource=admin&replicaSet=atlas-aont1m-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 mongo = PyMongo(app)
+client = pymongo.MongoClient('mongodb+srv://readonly:2kOAcow4qM6E4ANE@cluster0.6oig2.mongodb.net/test?authSource=admin&replicaSet=atlas-aont1m-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+db = client['stocks']
 
 # Model
 model = tf.keras.models.load_model('saved_model')
 model.build()
 print(model.summary())
 
-# Data Test
-# test = [[2.0170000e+03, 1.0000000e+00, 3.0000000e+00, 1.1634000e+02,
-#         1.1671000e+02, 2.0719500e+05, 0.0000000e+00, 0.0000000e+00,
-#         0.0000000e+00, 0.0000000e+00, 1.0000000e+00, 0.0000000e+00]]
-# testResult = model.predict(test).flatten()
-# print('---------------------------------------------------------------------------------------------------------------')
-# print(testResult)
-
 # Routes
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/date/', defaults={
-    'year': thisYear,
-    'month': thisMonth,
-    'day': thisDay
-})
-@app.route('/date/<year>/<month>/<day>/', methods=['GET','POST'])
-def datePage():
-    return render_template("date.html")
+@app.route('/date/')
+def mongoDatabase():
+    collection = db['car_stocks_2017']
+    data = list(collection.find())
+    return json.dumps(data, default = json_util.default)
+
 
 # Debug
 if __name__ == "__main__":
