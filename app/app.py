1a27fead88b4e6ca21e4e5dddb05047b6dc640a4
Jacob de Vries <mrjacobdevries@gmail.com>
2021-06-05 11:46:18 -0700

    Changes to routing.

---
+++ app/app.py
@@ -0,0 +1,21 @@
+# Dependencies
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+from flask_cors import CORS
+import tensorflow as tf
+
+# App
+app = Flask(__name__)
+CORS(app)
+
+# Model
+model = tf.keras.model.load_model('')
+print(model.summary())
+
+# Routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# Debug
+if __name__ == "__main__":
+    app.run(debug=True)
